219c1c066940a467e5ef92505e89513406313a56
CarolinaRVB <carolinavbastos@hotmail.com>
2025-06-07 13:06:56 +0100

    fixed bugs

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -9,15 +9,22 @@ if [ ! -d "/var/lib/mysql/maria_db" ]; then
     DB_PASSWORD=$(cat "$MYSQL_PASSWORD_FILE")
     DB_ROOT_PASSWORD=$(cat "$MYSQL_ROOT_PASSWORD_FILE")
 
-    # Replace template placeholders
+    # Replace template placeholders on init.sql with the variables
+    # and save output on tmp_sql
     export DB_NAME DB_USER DB_PASSWORD DB_ROOT_PASSWORD
     envsubst < "$INIT_SQL" > "$TMP_SQL"
 
+    # start MariaDB and wait for full start
     service mariadb start > /dev/null
     sleep 5
+    # execute the temporary file as root to :
+    # create database, user and set permissions
     mariadb -u root < "$TMP_SQL"
+    #stop mariadb
     service mariadb stop > /dev/null
+    # clean files
     rm -f "$INIT_SQL" "$TMP_SQL"
 fi
 
+# mysqld_safe helps keep the database running continuously and more stable
 exec mysqld_safe > /dev/null;
